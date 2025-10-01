022dc866658ff4749a1040337d422fa01db846fb
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-01 23:42:48 +0530

    initial server setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+
+app.listen(port, ()=>console.log(`Server is running on PORT: ${port}`))
