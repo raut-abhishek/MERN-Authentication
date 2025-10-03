592216970fc3c9c0d3c88dd48f877f09ed8503a0
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-03 14:47:37 +0530

    existing user controller function implemented

---
+++ server/controllers/authController.js
@@ -0,0 +1,36 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/userModel';
+
+
+
+
+
+
+
+export const register = async (req, res)=>{
+    const {name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        return res.json({success: false, message:'Missing Details'})
+    }
+
+    try {
+
+        const existingUser = await userModel.findOne({email});
+        if(existingUser){
+            return res.json({ success: false, message: 'User already exists'})
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new userModel({name, email, password: hashedPassword});
+        await user.save();
+
+        
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+
+}
