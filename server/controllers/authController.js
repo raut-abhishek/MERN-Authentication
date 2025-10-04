564a5f330208301fa75d2ab6f60664bbcddf117a
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-04 16:13:59 +0530

    reegister welcome email configuration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import pkg from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js';
 
 const { sign, verify } = pkg;
 
@@ -36,6 +37,20 @@ export const register = async (req, res)=>{
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7*24*60*60*1000
         })
+
+        // send welcome email config
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'Welcome to App',
+            text: `Welcome to website. Your account has been created with email id: ${email}`
+        }
+
+        await transporter.sendMail(mailOptions);
+        
+
+
+
 
         return res.json({success: true})
 
