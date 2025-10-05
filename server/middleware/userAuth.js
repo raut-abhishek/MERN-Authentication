4c75e94ab6be1e7d0a7d94db83780361517f4fc7
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-05 19:43:05 +0530

    userAuth middleware

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+const userAuth = async (req, res, next) => {
+    const { token } = req.cookies;
+
+    if (!token) {
+        return res.json({ success: false, message: 'Not authorised, try again' });
+    }
+
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (tokenDecode.id) {
+            req.userId = tokenDecode.id;
+        } else {
+            return res.json({ success: false, message: 'Not authorised, login again' });
+        }
+        next();
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+};
+
+export default userAuth;
