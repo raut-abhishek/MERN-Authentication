d2c8b91c81ea8024dc378213f3b73f1bb149d137
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-07 23:57:57 +0530

    creared AIP end points

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from './routes/authRoutes.js'
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,7 +13,9 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}));
+app.use(cors({
+    origin: "http://localhost:4000",
+    credentials: true}));
 
 app.get('/', (req, res)=>{
     res.send("Hello")
@@ -20,6 +23,7 @@ app.get('/', (req, res)=>{
 
 // API Endpoints
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 
 app.listen(port, ()=>console.log(`Server is running on PORT: ${port}`))
