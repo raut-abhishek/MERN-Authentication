5abee7fe888302aeb3ec59c852edfb22a2f1d26b
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-10-11 22:36:05 +0530

    added custom email template for email sending feature

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,12 +2,10 @@ import bcrypt from 'bcryptjs';
 import pkg from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 import transporter from '../config/nodemailer.js';
+import {EMAIL_VERIFY_TEMPLATE, PASSWORD_RESET_TEMPLATE} from '../config/emailTemplates.js'
+
 
 const { sign, verify } = pkg;
-
-
-
-
 
 export const register = async (req, res)=>{
     const {name, email, password} = req.body;
@@ -138,7 +136,8 @@ export const sendVerifyOtp = async (req, res)=>{
             from: process.env.SENDER_EMAIL,
             to: user.email,
             subject: 'Account Verification OTP',
-            text: `Your secure verification code is ${otp}. Enter this code to verify your account. ⚠️ For your safety, do not share this OTP with anyone.`
+            // text: `Your secure verification code is ${otp}. Enter this code to verify your account. ⚠️ For your safety, do not share this OTP with anyone.`,
+            html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}", otp).replace("{{email}}", user.email)
         }
 
         await transporter.sendMail(mailOption);
@@ -225,7 +224,8 @@ export const sendResetOtp = async (req, res)=>{
             from: process.env.SENDER_EMAIL,
             to: user.email,
             subject: 'Password reset OTP',
-            text: `Your secure password reset code is ${otp}. Enter this code to reset your password. ⚠️ For your safety, do not share this OTP with anyone.`
+            // text: `Your secure password reset code is ${otp}. Enter this code to reset your password. ⚠️ For your safety, do not share this OTP with anyone.`,
+            html: PASSWORD_RESET_TEMPLATE.replace("{{otp}}", otp).replace("{{email}}", user.email)
         }
 
         await transporter.sendMail(mailOption);
